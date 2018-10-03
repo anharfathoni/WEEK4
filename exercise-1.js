8ca32fdc4f850916e6cdbb93f6e73c98d7994d89
Anhar <anharqwerty@gmail.com>
2018-10-03 18:45:42 +0700

    files week 4

---
+++ exercise-1.js
@@ -0,0 +1,23 @@
+//Exercise 1 - Angka Prima
+
+function angkaPrima(angka) {
+    checkPrima = true
+    if (angka === 2) {
+        return true
+    } else {
+        for (i = 2; i < angka - 1; i++) {
+            if (angka % i === 0) {
+                checkPrima = false
+            }
+        }
+    }
+    return checkPrima
+}
+
+// TEST CASES
+//Angka yang dicek harus lebih besar sama dengan 2
+console.log(angkaPrima(3)); // true
+console.log(angkaPrima(7)); // true
+console.log(angkaPrima(6)); // false
+console.log(angkaPrima(23)); // true
+console.log(angkaPrima(33)); // false
